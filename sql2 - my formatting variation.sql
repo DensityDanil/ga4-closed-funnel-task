f370439294ab5aad82f9cced4a59530b0eae6705
Bristol Zoo <danpodlesniy@gmail.com>
2024-06-02 16:35:12 +0300

    feat: update funnel steps

--- sql2 - my formatting variation.sql
+++ sql2 - my formatting variation.sql
@@ -3,6 +3,19 @@
 with ga4_data AS (
 SELECT *
 FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_2020*` 
+WHERE user_pseudo_id='84534898.7989426950' 
+)
+
+
+,select_item AS (
+SELECT 
+      device.category AS category
+     ,event_name
+     ,(SELECT value.int_value FROM UNNEST(event_params) WHERE event_name = 'select_item' AND key = 'ga_session_id') AS step0_id,
+  FROM
+    ga4_data
+  WHERE
+    event_name = "select_item"
 )
 
 ,view_item AS (
@@ -16,13 +29,13 @@ SELECT
     event_name = "view_item"
 )
 
-,begin_checkout AS (
+,add_to_cart AS (
   SELECT
-     (SELECT value.int_value FROM UNNEST(event_params) WHERE event_name = 'begin_checkout' AND key = 'ga_session_id') AS step2_id
+     (SELECT value.int_value FROM UNNEST(event_params) WHERE event_name = 'add_to_cart' AND key = 'ga_session_id') AS step2_id
   FROM
     ga4_data
   WHERE
-    event_name = "begin_checkout" 
+    event_name = "add_to_cart" 
 )
 
 
@@ -37,10 +50,11 @@ SELECT
 
 
 ,funnel_conditions AS (
-SELECT      t1.*,t2.*,t3.*
-FROM        view_item       t1 
-LEFT JOIN   begin_checkout  t2 ON step2_id = step1_id
-LEFT JOIN   purchase        t3 ON step3_id = step1_id
+SELECT      t0.*,t1.*,t2.*,t3.*
+FROM        select_item     t0
+LEFT JOIN   view_item       t1 ON step2_id = step0_id
+LEFT JOIN   add_to_cart     t2 ON step2_id = step0_id
+LEFT JOIN   purchase        t3 ON step3_id = step0_id
 
 )
 
